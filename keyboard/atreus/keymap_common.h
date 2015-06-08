bbd3e8aa0b317b344f35921121be01d789c35b6e
Chris Keathley <c@keathley.io>
2015-06-07 23:51:44 -0400

    Add media and pane navigation layer

--- keyboard/atreus/keymap_common.h
+++ keyboard/atreus/keymap_common.h
@@ -33,6 +33,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define CTRL(key) ACTION(ACT_MODS, (MOD_LCTL << 8) | (key))
 #define ALT(key) ACTION(ACT_MODS, (MOD_LALT << 8) | (key))
 #define GUI(key) ACTION(ACT_MODS, (MOD_LGUI << 8) | (key))
+#define PANE(key) ACTION_MODS_KEY(MOD_LGUI | MOD_LALT | MOD_LCTL, (key))
 
 extern const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS];
 extern const uint16_t fn_actions[];
@@ -51,9 +52,9 @@ extern const uint16_t fn_actions[];
 }
 
 #define FN_LAYER   KEYMAP(SHIFT(KC_1), SHIFT(KC_2), SHIFT(KC_LBRC), SHIFT(KC_RBRC), SHIFT(KC_BSLS), \
-                          KC_PGUP, KC_7, KC_8, KC_9, SHIFT(KC_8),       \
+                          SHIFT(CTRL(KC_UP)), KC_7, KC_8, KC_9, SHIFT(KC_8),       \
                           SHIFT(KC_3), SHIFT(KC_4), SHIFT(KC_9), SHIFT(KC_0), KC_GRAVE, \
-                          KC_PGDN, KC_4, KC_5, KC_6, SHIFT(KC_EQUAL),   \
+                          SHIFT(CTRL(KC_DOWN)), KC_4, KC_5, KC_6, SHIFT(KC_EQUAL),   \
                           SHIFT(KC_5), SHIFT(KC_6), KC_LBRC, KC_RBRC, SHIFT(KC_GRAVE), \
                           SHIFT(KC_7), KC_1, KC_2, KC_3, KC_BSLS,       \
                           KC_FN1, SHIFT(KC_INS), KC_LALT, KC_LSFT, KC_BSPC, KC_LGUI, KC_FN4, \
@@ -63,6 +64,16 @@ extern const uint16_t fn_actions[];
                          KC_LEFT, KC_DOWN, KC_RIGHT, KC_PGDN, KC_PGDN, KC_DOWN, KC_F4, KC_F5, KC_F6, KC_F11, \
                          KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_F1, KC_F2, KC_F3, KC_F12, \
                          KC_NO, KC_NO, KC_LALT, KC_LSFT, KC_BSPC, KC_LGUI, KC_FN4, KC_SPC, KC_FN2, KC_NO, KC_NO, KC_FN3)
+
+#define MEDIA_LAYER KEYMAP(KC_SLEP, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_MUTE, KC_VOLD, KC_VOLU, \
+                           KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_MPRV, KC_MPLY, KC_MNXT, \
+                           KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
+                           KC_FN6, KC_FN5, KC_LALT, KC_LSFT, KC_BSPC, KC_LGUI, KC_FN4, KC_SPC, KC_FN2, KC_NO, KC_NO, KC_NO)
+
+#define PANE_LAYER KEYMAP(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_UP, KC_NO, KC_NO, \
+                          KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_LEFT, KC_DOWN, KC_RIGHT, KC_NO, \
+                          KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, PANE(KC_LEFT), PANE(KC_DOWN), PANE(KC_UP), PANE(KC_RIGHT), KC_NO, \
+                          KC_FN6, KC_FN5, KC_LALT, KC_LSFT, KC_BSPC, KC_LGUI, KC_FN4, KC_SPC, KC_FN2, KC_NO, KC_NO, KC_NO)
 
 enum function_id {
   BOOTLOADER,
--- keyboard/atreus/keymap_qwerty.c
+++ keyboard/atreus/keymap_qwerty.c
@@ -5,20 +5,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KEYMAP(KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, \
          KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, \
          KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, \
-         KC_ESC, KC_LALT, GUI(KC_TAB), KC_LSFT, KC_BSPC, KC_LGUI, KC_FN4,     \
+         KC_FN6, KC_FN5, KC_LALT, KC_LSFT, KC_BSPC, KC_LGUI, KC_FN4,     \
          KC_SPC, KC_FN0, KC_MINS, KC_QUOT, KC_ENT),                     \
   /* 1: punctuation and numbers */
   FN_LAYER,                                     \
   /* 2: arrows and function keys */
-  LAYER_TWO
+  LAYER_TWO,
+  MEDIA_LAYER,
+  PANE_LAYER
 };
+
+
 
 const uint16_t PROGMEM fn_actions[] = {
   [0] = ACTION_LAYER_MOMENTARY(1),  // to Fn overlay
   [1] = ACTION_LAYER_ON(2, 1),  // switch to layer 2
   [2] = ACTION_LAYER_OFF(2, 1),  // switch back to layer 0
   [3] = ACTION_FUNCTION(BOOTLOADER),
-  [4] = ACTION_MODS_TAP_KEY(MOD_LCTL, KC_TAB)
+  [4] = ACTION_MODS_TAP_KEY(MOD_LCTL, KC_TAB), // ctrl or tab
+  [5] = ACTION_LAYER_MOMENTARY(3), // Media
+  [6] = ACTION_LAYER_TAP_KEY(4, KC_ESC) // Pane toggle
 };
 
 void action_function(keyrecord_t *record, uint8_t id, uint8_t opt)
